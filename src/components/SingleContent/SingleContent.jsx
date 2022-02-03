d75a3850b7881d129177f152b0bff0d374c99813
Shruti Vaidya <vaidya.shruti1108@gmail.com>
2022-02-03 16:14:52 +0545

    content modal added

--- src/components/SingleContent/SingleContent.jsx
+++ src/components/SingleContent/SingleContent.jsx
@@ -1,18 +1,19 @@
 import { img_300, unavailable } from '../../config/config';
 import './singlecontent.css';
 import { Badge } from '@material-ui/core';
+import ContentModal from '../modal/Modal';
 
 const SingleContent = ({ id, poster, title, date, media_type, vote_average, }) => {
 
   return (
-  <div className='media'>
+  <ContentModal id={id} media_type={media_type}>
     <Badge badgeContent={vote_average} color= {vote_average>7 ? 'primary' : 'secondary'} />
     <img className='images' src={poster? `${img_300}/${poster}` : unavailable} />
     <b className='title'>{title}</b>
     <span className='subtitle'>{media_type === 'tv' ? 'TV Series' : 'Movie'}
     <span className='subtitle'>{date}</span> 
     </span>
-  </div>
+  </ContentModal>
   );
 };
 
